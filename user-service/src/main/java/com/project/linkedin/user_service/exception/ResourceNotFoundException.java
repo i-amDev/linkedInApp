82dffce3be5153e7517eb27b433f23583f93cdbb
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-19 15:47:07 +0530

    - Added the config file. - Implemented the custom ResourceNotFoundException, BadRequestException classes. - Implemented a GlobalExceptionHandler class to catch all the exceptions. - Implemented the ApiError class to gracefully handle the exception format.

---
+++ user-service/src/main/java/com/project/linkedin/user_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.project.linkedin.user_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException (String message) {
+        super(message);
+    }
+}
