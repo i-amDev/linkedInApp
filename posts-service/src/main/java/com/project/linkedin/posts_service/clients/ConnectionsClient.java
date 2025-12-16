a39996aa74c83408c7d75d6f01903f9825866ba9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-12-16 15:48:45 +0530

    - Refactored. - Removed unused import.

--- posts-service/src/main/java/com/project/linkedin/posts_service/clients/ConnectionsClient.java
+++ posts-service/src/main/java/com/project/linkedin/posts_service/clients/ConnectionsClient.java
@@ -2,7 +2,6 @@ package com.project.linkedin.posts_service.clients;
 
 import com.project.linkedin.posts_service.dto.PersonDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
