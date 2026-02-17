a30a87f60de91cea576425acbc4c8ac50a063748
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-02-17 15:11:58 +0530

    - Refactored.

--- posts-service/src/main/java/com/project/linkedin/posts_service/auth/UserInterceptor.java
+++ posts-service/src/main/java/com/project/linkedin/posts_service/auth/UserInterceptor.java
@@ -23,5 +23,4 @@ public class UserInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         UserContextHolder.clear();
     }
-
 }
